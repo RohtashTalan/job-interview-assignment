aa421a269c5d7b4ddfc08fdcea45ce4620740e69
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-06 16:57:35 +0530

    controller file and routes

---
+++ NodeSure_Web-Development/backend/middleware/auth.middleware.js
@@ -0,0 +1,5 @@
+import  Jwt, { JsonWebTokenError }  from "jsonwebtoken";
+import User from '../schema/users.schema';
+import config from "../config";
+import CustomError from "../utils/customError";
+
