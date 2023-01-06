8be2443abd4dcf37f50d01920b62e97e17cfa937
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-06 15:48:21 +0530

    correction in json file

---
+++ NodeSure_Web-Development/Assignment.js
@@ -0,0 +1,18 @@
+/* Description:
+
+Create a frontend application (in any javascript framework) for logins and listing items stored in a database.
+Only those users can log in to the application that exists in our DB. And, after logging list all product items in tabular format with pagination.
+Users can select the product and it can store in session. So, they can get them once they are logged in back.
+
+Tech Stack:
+PHP for Building Rest APIs (if not familiar with PHP then create APIs in Node.js)
+Any Javascript framework to build a frontend application. Vue.js preferable.
+The database would be MongoDB
+
+Resources:
+The attached assignmentdb_.zip is the database dump for this assignment.
+
+Submission:
+Please provide a git repo link for project submission.
+
+*/
